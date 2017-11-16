7a52bf5e819be8ae2d43fc372ecd3ed7e5806931
christelletalbot <christelletalbot12@gmail.com>
2017-11-16 14:08:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Javascript");
+function add(a,b){
+return(a+b);
+}
+console.log(add(48,12));
+
+function sub(a,b){
+return a-b;	
+}
+console.log(sub(25,10));
+
+function div(a,b){
+	if(b===0){
+		return "fatal";
+	}else {
+		return a/b;	
+	}	
+}
+console.log(div(50,10));
+
